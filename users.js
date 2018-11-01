e5c5b9390475e1433542f2cb6a7ee3afad2f156b
Kevin Black <kevin.black@wsu.edu>
2018-10-31 20:33:42 -0700

    Added databases, homepage logos, and homepage file

---
+++ users.js
@@ -0,0 +1,14 @@
+const users = [
+	{
+		"EMAIL": "admin",
+		"PASSWORD": "password"
+	},
+	{
+		"EMAIL": "kevin.black@wsu.edu",
+		"PASSWORD": "password",
+		"NAME": "Kevin Black",
+		"BILLING_ADD": "0000 NE 000 Ave",
+		"CITY": "Vancouver",
+		"STATE": "WA"
+	}
+];
